142e11020db78591a563db09db441413fe6d8968
AN-Xiang-yu <62239241+AN-Xiang-yu@users.noreply.github.com>
2021-01-13 16:30:53 +0100

    connexion kotllin bases de données

--- Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
+++ Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
@@ -16,52 +16,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import java.sql.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_hello.setOnClickListener {
+
+
+        afficherResultat.setOnClickListener {
             Thread(Runnable {
-                val sql = "SELECT value FROM Temperature"
+                val sql = "SELECT * FROM Temperature;"
                 mysqlConnection(sql)
-           }).start()
+            }).start()
         }
     }
 
-    /**
-     * 连接数据库
-     */
-    fun mysqlConnection(sql:String) {
+    fun mysqlConnection(sql: String) {
         var cn: Connection
+
         try {
-            //加载驱动
-            Class.forName("com.mysql.jdbc.Driver")
-            //建立连接
-            Log.d("connnexion","****************************connnexion*****************************")
-            cn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/neunoeiltest",
-                "appli1", "#M0td3p@553")
-            val ps = cn.createStatement()
-            val resultSet = ps!!.executeQuery(sql)
-            while (resultSet.next()) {
-                Log.d("mysqlConnection: " , resultSet.getString("id") +
-                        resultSet.getString("name") +
-                        resultSet.getString("password")+resultSet.getString("email"))
+            Class.forName("com.mysql.jdbc.Driver")//Définir le système de démarrage de MySQL
+            cn = DriverManager.getConnection(
+                "jdbc:mysql://db4free.net:3306/neunoeiltest",
+                "appli1", "#M0td3p@553"
+            )//Etablir la connexion
+
+            var stmt = cn.createStatement()//Réaliser Statement pour exécuter le requete SQL
+            var resultSet = stmt!!.executeQuery(sql)//Recevoir le résultat
+            while (resultSet.next()) {//Afficher
+                Log.d(
+                    "mysqlConnection: ", resultSet.getString(1) + " " +
+                            resultSet.getString(2) + " " +
+                            resultSet.getString(3) + " " + resultSet.getString(4)
+                )
+                println(resultSet.getString(1))
             }
-            if (ps != null) {
-                ps!!.close()
+            if (stmt != null) {
+                stmt!!.close()
             }
             if (cn != null) {
                 cn.close()
             }
-        } catch (e: ClassNotFoundException) {
-            e.printStackTrace()
-        } catch (e: SQLException) {
-            e.printStackTrace()
+        } catch (ex: SQLException) {
+            //handle any errors
+            ex.printStackTrace()
+        } catch (ex: Exception) {
+            //handle any errors
+            ex.printStackTrace()
         }
-
     }
 }

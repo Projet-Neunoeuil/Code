8ca936e058c5ae9b972692b2d3d4e6adcbcca235
AN-Xiang-yu <62239241+AN-Xiang-yu@users.noreply.github.com>
2021-01-14 12:08:09 +0100

    connexion kotlin bases de données essai d'utiliser asyncTask 1

--- Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
+++ Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.aqualala
 
-import android.os.Build
 import android.os.Bundle
-import android.os.StrictMode
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
@@ -14,36 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (Build.VERSION.SDK_INT > 9) {
-            val policy =
-                StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
-        }
+
         var resultatAffiche: TextView = findViewById(R.id.renvois)
         var bouton: Button = findViewById(R.id.afficherResultat)
         bouton.setOnClickListener {
-            Thread(Runnable {
+
                 val sql = "SELECT * FROM Temperature;"
                 resultatAffiche.text=mysqlConnection(sql)
-            }).start()
+
         }
     }
 
     fun mysqlConnection(sql: String) : String? {
-        var cn: Connection
         var renv: String? = ""
 
         try {
-            Class.forName("com.mysql.jdbc.Driver")//Définir le système de démarrage de MySQL
-            cn = DriverManager.getConnection(
+            Class.forName("com.mysql.cj.jdbc.Driver")//Définir le système de démarrage de MySQL
+            var cn = DriverManager.getConnection(
                 "jdbc:mysql://db4free.net:3306/neunoeiltest",
                 "appli1", "#M0td3p@553"
             )//Etablir la connexion
-
             var stmt = cn.createStatement()//Réaliser Statement pour exécuter le requete SQL
             var resultat = stmt!!.executeQuery(sql)//Recevoir le résultat
             while (resultat.next()) {//Afficher
-                renv += """+${resultat.getString(1)}${resultat.getString(2)}"""
+                renv += resultat.getString(1)+" "+resultat.getString(2)+" \n"
             }
             if (stmt != null) {
                 stmt!!.close()

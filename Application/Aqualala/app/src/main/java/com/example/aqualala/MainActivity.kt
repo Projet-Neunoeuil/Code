300b16b083fbf2154c1d59f74949e12e7a32d452
AN-Xiang-yu <62239241+AN-Xiang-yu@users.noreply.github.com>
2021-01-15 14:42:07 +0100

    connexion améliorée kotlin bases de données

--- Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
+++ Application/Aqualala/app/src/main/java/com/example/aqualala/MainActivity.kt
@@ -1,54 +1,57 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.aqualala
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
 import java.sql.DriverManager
-import java.sql.SQLException
 
 class MainActivity : AppCompatActivity() {
+    var resulatAfficher: TextView? = null
+    var boutonAfficher: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var resultatAffiche: TextView = findViewById(R.id.renvois)
-        var bouton: Button = findViewById(R.id.afficherResultat)
-        bouton.setOnClickListener {
-
-                val sql = "SELECT * FROM Temperature;"
-                resultatAffiche.text=mysqlConnection(sql)
-
-        }
+        resulatAfficher = findViewById<View>(R.id.affichage) as TextView
+        boutonAfficher = findViewById<View>(R.id.bouton) as Button
+        boutonAfficher!!.setOnClickListener { Async().execute() }
     }
 
-    fun mysqlConnection(sql: String) : String? {
-        var renv: String? = ""
+    internal inner class Async: AsyncTask<Void?, Void?, Void?>(){
+        //Résulat envoyé
+        var renvoi = ""
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver")//Définir le système de démarrage de MySQL
-            var cn = DriverManager.getConnection(
-                "jdbc:mysql://db4free.net:3306/neunoeiltest",
-                "appli1", "#M0td3p@553"
-            )//Etablir la connexion
-            var stmt = cn.createStatement()//Réaliser Statement pour exécuter le requete SQL
-            var resultat = stmt!!.executeQuery(sql)//Recevoir le résultat
-            while (resultat.next()) {//Afficher
-                renv += resultat.getString(1)+" "+resultat.getString(2)+" \n"
+        override fun doInBackground(vararg params: Void?): Void? {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
+                //Connection avec MySQL avec url,user, mode de pass
+                var cn = DriverManager.getConnection(
+                    //jdbc:mysql://<IP ou nom de domaine>:<port>/<nom de BD>
+                    "jdbc:mysql://193.26.21.39:3306/Application",
+                    "Appli","#M0td3p@553"
+                )
+                //
+                var stmt = cn.createStatement()//Réaliser Statement pour exécuter le requete SQL
+                var resultat = stmt.executeQuery("SELECT * FROM Temperature")//Recevoir le résultattat
+                if (resultat.next()) {
+                    renvoi += resultat.getString("value")+" "+resultat.getString("time")+" "+resultat.getBoolean("inRange")
+                }
+            } catch (ex: Exception) {
+                renvoi = ex.toString()
             }
-            if (stmt != null) {
-                stmt!!.close()
-            }
-            if (cn != null) {
-                cn.close()
-            }
-        } catch (ex: SQLException) {
-            //handle any errors
-            return ex.toString()
-        } catch (ex: Exception) {
-            //handle any errors
-            return ex.toString()
+            return null
         }
-        return renv
+
+        override fun onPostExecute(aVoid: Void?) {
+            resulatAfficher!!.text= renvoi
+            super.onPostExecute(aVoid)
+        }
     }
 }

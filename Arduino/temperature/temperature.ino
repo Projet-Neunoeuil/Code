f698d38d181a972074c5c436d72ce58e761f2e4c
Rapha <raphael.baulant@etu.unilim.fr>
2021-01-16 12:16:52 +0100

    code arduino temperature

---
+++ Arduino/temperature/temperature.ino
@@ -0,0 +1,22 @@
+const int sensorPin = A0;
+
+void setup() {
+  // open a serial connection to display values
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value on AnalogIn pin 0 and store it in a variable (10-bit sensor value)
+  int sensorVal = analogRead(sensorPin);
+
+  // convert the ADC reading to voltage
+  float voltage = (sensorVal / 1024.0) * 5.0;
+
+  // convert the voltage to temperature in degrees C
+  // the sensor changes 10 mV per degree
+  // the datasheet says there's a 500 mV offset
+  float temperature = (voltage - .5) * 100;
+  Serial.print(temperature);
+
+  delay(10000);
+}

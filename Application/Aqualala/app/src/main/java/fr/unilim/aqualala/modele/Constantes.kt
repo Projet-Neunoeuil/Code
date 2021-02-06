a63c1d87a6258387e734f4105b4b14a13fd93c26
AN-Xiang-yu <62239241+AN-Xiang-yu@users.noreply.github.com>
2021-02-06 19:33:59 +0100

    Changemenr de BD

--- Application/Aqualala/app/src/main/java/fr/unilim/aqualala/modele/Constantes.kt
+++ Application/Aqualala/app/src/main/java/fr/unilim/aqualala/modele/Constantes.kt
@@ -3,9 +3,9 @@ package fr.unilim.aqualala.modele
 class Constantes {
     companion object {
         //Constants pour la connexion de base de données
-        val IP = "avalgan.ch"
+        val IP = "xiangyu-an.fr"
         val PORT = "3306"
-        val NOM_BASE_DONNEES = "TempNeunoeil"
+        val NOM_BASE_DONNEES = "Application"
         val USER = "Appli"
         val PASSEWORD = "#M0td3p@553"
         //Constant pour l'environnement de poisson
